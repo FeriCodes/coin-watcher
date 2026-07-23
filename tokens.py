5aefc874e1814ae35e01d78fc23769113e8c1444
Feri <hosseinbigdeli335@gmail.com>
2026-07-24 00:16:37 +0330

    feat: add token configuration file

---
+++ tokens.py
@@ -0,0 +1,29 @@
+TOKENS = {
+    "bitcoin": {
+        "type": "id",
+        "value": "bitcoin",
+    },
+    "ethereum": {
+        "type": "id",
+        "value": "ethereum",
+    },
+    "defi": {
+        "type": "contract",
+        "network": "ton",
+        "value": "0x6d106c0b8d2f47c5465bdbd58d1be253762cbbc1",
+    },
+    "dogs": {
+        "type": "contract",
+        "network": "ton",
+        "value": "EQCvxJy4eG8hyHBFsZ7eePxrRsUQSFE_jpptRAYBmcG_DOGS",
+    },
+    "toncoin": {
+        "type": "contract",
+        "network": "ton",
+        "value": "EQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAM9c",
+    },
+    "notcoin": {
+        "type": "id",
+        "value": "notcoin",
+    },
+}
