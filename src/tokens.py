f86cd09a5bb1da368f536c5c28847e248ba1d831
Feri <hosseinbigdeli335@gmail.com>
2026-07-30 21:48:47 +0330

    feat: Add 3 new tokens to the tokens dictionary

--- src/tokens.py
+++ src/tokens.py
@@ -12,4 +12,19 @@ TOKENS = {
         "value": "notcoin",
     },
     "helium": {"type": "id", "value": "helium"},
+    "gram": {
+        "type": "contract",
+        "value": "EQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAM9c",
+        "network": "ton",
+    },
+    "dogs": {
+        "type": "contract",
+        "value": "EQCvxJy4eG8hyHBFsZ7eePxrRsUQSFE_jpptRAYBmcG_DOGS",
+        "network": "ton",
+    },
+    "defi": {
+        "type": "contract",
+        "value": "0x6d106c0b8d2f47c5465bdbd58d1be253762cbbc1",
+        "network": "bsc",
+    },
 }
