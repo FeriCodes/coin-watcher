1a2cb4b78fb2ea57f805d70830351aad7f733992
Feri <hosseinbigdeli335@gmail.com>
2026-07-31 22:22:35 +0330

    feat(tokens): add name attribute for UI and fix De.Fi token type

--- src/tokens.py
+++ src/tokens.py
@@ -1,28 +1,33 @@
 TOKENS = {
     "bitcoin": {
+        "name": "Bitcoin",
         "type": "id",
         "value": "bitcoin",
     },
     "ethereum": {
+        "name": "Ethereum",
         "type": "id",
         "value": "ethereum",
     },
     "notcoin": {
+        "name": "Notcoin",
         "type": "id",
         "value": "notcoin",
     },
-    "helium": {"type": "id", "value": "helium"},
-    "gram": {
-        "type": "contract",
-        "value": "EQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAM9c",
-        "network": "ton",
+    "helium": {"name": "Helium", "type": "id", "value": "helium"},
+    "the-open-network": {
+        "name": "TON",
+        "type": "id",
+        "value": "the-open-network",
     },
     "dogs": {
+        "name": "DOGS",
         "type": "contract",
         "value": "EQCvxJy4eG8hyHBFsZ7eePxrRsUQSFE_jpptRAYBmcG_DOGS",
         "network": "ton",
     },
-    "defi": {
+    "de-fi": {
+        "name": "De.Fi",
         "type": "contract",
         "value": "0x6d106c0b8d2f47c5465bdbd58d1be253762cbbc1",
         "network": "bsc",
